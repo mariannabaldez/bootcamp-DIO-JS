8e46d908dc825b0f784bdeb4c2efbc1f050fb04f
mariannabaldez <marianna.baldez@gmail.com>
2022-06-15 15:40:21 -0300

    primeiro site em php

---
+++ hello-world.php
@@ -0,0 +1,12 @@
+<html lang="pt">
+<head>
+    <title>Hello world!</title>
+</head>
+<body>
+    <?php
+        for ($numero = 0; $numero < 10; $numero ++){
+            print("linha número " . $numero . </br>);
+        }
+    ?>
+</body>
+</html>
